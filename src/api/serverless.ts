2b47e84125bfaa66567b9961840bbf1c367623e8
Joshua Snyder <joshua@timenavi.com>
2023-06-02 21:51:19 +0100

    fix type errors

--- src/api/serverless.ts
+++ src/api/serverless.ts
@@ -1,21 +1,22 @@
-"use strict";
+'use strict'
 
 // Read the .env file.
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 // Require the framework
-import Fastify from "fastify";
+import Fastify from 'fastify'
 
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
-});
+})
 
 // Register your application as a normal plugin.
-app.register(import("../app"));
+// @ts-ignore
+app.register(import('../app'))
 
-export default async (req, res) => {
-    await app.ready();
-    app.server.emit('request', req, res);
+export default async (req: Request, res: Response) => {
+  await app.ready()
+  app.server.emit('request', req, res)
 }
