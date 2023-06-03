41646bf7a593cba49e74ad7b27986c6f365881f0
Joshua Snyder <joshua@timenavi.com>
2023-06-03 10:09:25 +0100

    fix serverless.ts route

--- api/serverless.ts
+++ api/serverless.ts
@@ -5,18 +5,35 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 // Require the framework
-import Fastify from 'fastify'
+import Fastify, { FastifyInstance } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+import { createServer } from '../src/app'
+import pino from 'pino'
 
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 })
 
-// Register your application as a normal plugin.
-// @ts-expect-error - import thing
-app.register(import('../src/app'))
+const logger = pino({
+  formatters: {
+    level(label) {
+      return { level: label }
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+})
 
 export default async (req: Request, res: Response) => {
+  const customApp: FastifyInstance<Server, IncomingMessage, ServerResponse> = await createServer({
+    logger,
+    exposeDocs: process.env.NODE_ENV !== 'production',
+    requestIdHeader: 'Request-Id',
+  })
+
+  // Register your application as a normal plugin.
+  // @ts-expect-error - import thing
+  app.register(customApp)
   await app.ready()
   app.server.emit('request', req, res)
 }
